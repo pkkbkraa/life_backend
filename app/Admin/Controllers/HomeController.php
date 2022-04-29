08206d12bc5b3d411b143833c35739107cd71c28
Thomas <pkkbkraa@gmail.com>
2022-04-29 16:10:47 +0800

    no message

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -13,24 +13,6 @@ class HomeController extends Controller
 {
     public function index(Content $content)
     {
-        return $content
-            ->header('Dashboard')
-            ->description('Description...')
-            ->body(function (Row $row) {
-                $row->column(6, function (Column $column) {
-                    $column->row(Dashboard::title());
-                    $column->row(new Examples\Tickets());
-                });
-
-                $row->column(6, function (Column $column) {
-                    $column->row(function (Row $row) {
-                        $row->column(6, new Examples\NewUsers());
-                        $row->column(6, new Examples\NewDevices());
-                    });
-
-                    $column->row(new Examples\Sessions());
-                    $column->row(new Examples\ProductOrders());
-                });
-            });
+        return admin_redirect('banner');
     }
 }
